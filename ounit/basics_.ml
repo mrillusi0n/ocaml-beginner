4cb9b08e0440f7e3f492e33f6b022b0324cc37e6
Nikhil CSB <csbnikhil@gmail.com>
2021-01-11 12:38:42 +0530

    test with multiple functions

---
+++ ounit/basics_.ml
@@ -0,0 +1,17 @@
+open OUnit2
+open Basics
+
+let double_tests = "test suite for double" >::: [
+  "zero"  >:: (fun _ -> assert_equal 0 (double 0));
+  "one"   >:: (fun _ -> assert_equal 2 (double 1));
+  "four"  >:: (fun _ -> assert_equal 8 (double 4));
+]
+
+let square_tests = "test suite for square" >::: [
+  "zero"  >:: (fun _ -> assert_equal 0 (square 0));
+  "one"   >:: (fun _ -> assert_equal 1 (square 1));
+  "four"  >:: (fun _ -> assert_equal 16 (square 4));
+]
+
+let _ = run_test_tt_main double_tests
+let _ = run_test_tt_main square_tests
