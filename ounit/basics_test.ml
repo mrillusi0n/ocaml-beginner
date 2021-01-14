e7d07d278da55f07650b028152829e3e023035a0
Nikhil CSB <csbnikhil@gmail.com>
2021-01-14 15:59:50 +0530

    cleanup

--- ounit/basics_test.ml
+++ ounit/basics_test.ml
@@ -1,16 +1,19 @@
 open OUnit2
 open Basics
 
+let make_test name f expected input =
+    name >:: (fun _ -> assert_equal expected (f input) ~printer:string_of_int)
+
 let double_tests = "test suite for double" >::: [
-  "zero"  >:: (fun _ -> assert_equal 0 (double 0) ~printer:string_of_int);
-  "one"   >:: (fun _ -> assert_equal 2 (double 1) ~printer:string_of_int);
-  "four"  >:: (fun _ -> assert_equal 8 (double 4) ~printer:string_of_int);
+  make_test "zero"  double 0 0;
+  make_test "one"   double 2 1;
+  make_test "four"  double 8 4;
 ]
 
 let square_tests = "test suite for square" >::: [
-  "zero"  >:: (fun _ -> assert_equal 0 (square 0) ~printer:string_of_int);
-  "-one"  >:: (fun _ -> assert_equal 1 (square (-1)) ~printer:string_of_int);
-  "four"  >:: (fun _ -> assert_equal 16 (square 4) ~printer:string_of_int);
+  make_test "zero"  square 0  0;
+  make_test "-one"  square 1 (-1);
+  make_test "four"  square 16 4;
 ]
 
 let _ = run_test_tt_main double_tests
