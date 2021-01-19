da5072de037ff90f90aaeb51f27eb085de2fe411
Nikhil CSB <csbnikhil@gmail.com>
2021-01-20 05:10:14 +0530

    more exercises

--- lists/std.ml
+++ lists/std.ml
@@ -4,3 +4,28 @@ let get_fifth lst =
 
 let sort_desc (lst : int list) =
     List.rev (List.sort Stdlib.compare lst)
+
+let rec last_item lst =
+    if List.tl lst = [] then List.hd lst
+    else last_item (List.tl lst)
+
+let any_zeroes =
+    List.exists (fun x -> x == 0)
+
+let rec take n lst =
+    match lst with
+    | []   -> []
+    | h::t -> if n = 0 then [] else h::(take (n-1) t)
+
+let rec t_take n lst =
+    match lst,n with
+    | [],_   -> []
+    | _,0    -> []
+    | h::t,_ -> h::(t_take (n-1) t)
+
+let rec drop n lst =
+    match lst,n with
+    | [],_   -> []
+    | lst,0  -> lst
+    | _::t,_ -> (drop (n-1) t)
+
