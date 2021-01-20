eb190f51c8be68c7539ddd3ced475c215fac8232
Nikhil CSB <csbnikhil@gmail.com>
2021-01-20 16:49:42 +0530

    naive powerset

---
+++ lists/powerset.ml
@@ -0,0 +1,8 @@
+let rec _powerset s = function
+    | [] -> s
+    | x::xs -> let t =
+        List.rev_map (List.cons x) s
+    in _powerset (List.rev_append t s) xs
+
+
+let powerset lst = _powerset [[]] lst
