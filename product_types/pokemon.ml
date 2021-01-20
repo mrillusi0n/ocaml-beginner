c9e59bf3ab75055134c8e2c51d47a90b2f87d55a
Nikhil CSB <csbnikhil@gmail.com>
2021-01-21 04:42:33 +0530

    no folding

--- product_types/pokemon.ml
+++ product_types/pokemon.ml
@@ -8,9 +8,10 @@ let pikachu = {name = "Pikachu"; hp = 64; ptype = Normal}
 let pokemons = [charizard; metapod; pikachu]
 
 let stronger_poke a b =
-    if a.hp > b.hp then a
-    else b
+    if a.hp > b.hp then a else b
 
-let max_hp = function
+let rec max_hp = function
     | [] -> None
-    | poke::pokes -> Some (List.fold_left stronger_poke poke pokes) 
+    | poke::pokes -> match max_hp pokes with
+        | Some m -> Some (stronger_poke poke m)
+        | None -> Some poke
