b98fbbbecb36caa72052c4a70c13f90725f9ce8f
Nikhil CSB <csbnikhil@gmail.com>
2021-01-29 17:44:44 +0530

    infinite data structures

---
+++ ads/streams.ml
@@ -0,0 +1,14 @@
+type 'a stream =
+    Cons of 'a * (unit -> 'a stream)
+
+let rec from n =
+    Cons (n, fun () -> from (n+1))
+
+let hd (Cons (x,_)) = x
+let tl (Cons (_,x)) = x ()
+
+let rec take n s =
+    if n = 0 then []
+    else hd s :: take (n-1) (tl s)
+
+let nats = from 0
