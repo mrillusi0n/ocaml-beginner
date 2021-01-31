96232a628dd69f2504341ec6e34cd89e08b1b983
Nikhil CSB <csbnikhil@gmail.com>
2021-01-31 19:12:57 +0530

    bst

---
+++ ads/bst.ml
@@ -0,0 +1,12 @@
+type 'a tree = Leaf | Node of 'a * 'a tree * 'a tree
+
+let rec mem x = function
+    | Leaf -> false
+    | Node (v,l,r) -> x = v || (x < v && mem v l) || mem v r
+
+let rec insert x  = function
+    | Leaf -> Node (x, Leaf, Leaf)
+    | Node (v,l,r) as t ->
+            if x = v then t
+            else if x < v then Node (v, insert x l, r)
+            else Node (v, l, insert x r)
