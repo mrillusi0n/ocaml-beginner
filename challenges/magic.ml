cefdb2261b5d2f0793051f8f6332644e10cb3047
Nikhil CSB <csbnikhil@gmail.com>
2021-02-12 04:30:45 +0530

    reverse fish compose ftw

--- challenges/magic.ml
+++ challenges/magic.ml
@@ -1,5 +1,14 @@
 let (<-<) f g x = x |> g |> f
 
+let magik =
+    List.map ((Stdlib.( * ) 2) <-< (Stdlib.( + ) 1))
+    <-< List.filter ((((<>) 0) <-< (Stdlib.(land) 1)))
+    <-< List.map abs
+    <-< List.map int_of_float
+    <-< List.map ((Fun.flip Stdlib.( ** )) 2.)
+    <-< List.map float_of_int
+    <-< List.filter ((>) 20)
+
 let magic lst =
     lst
     |> List.filter ((>) 20)
