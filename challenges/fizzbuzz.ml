d0442e0c6237a109a9e2b2cbb7a47f176123a330
mrillusi0n <csbnikhil@gmail.com>
2021-08-06 06:49:56 +0530

    add first iter of fizzbuzz

---
+++ challenges/fizzbuzz.ml
@@ -0,0 +1,28 @@
+let fizzbuzz num =
+    let rec say acc diags x = match diags with
+        | [] -> (match acc with
+            | [] -> None
+            | _ -> Some acc)
+        | (n,diag)::rest -> let new_acc =
+            if x mod n = 0 then diag::acc
+            else acc in
+            say new_acc rest x
+    in
+    let diags = [
+        (5, "Buzz");
+        (3, "Fizz");
+    ] in match say [] diags num with
+    | None -> string_of_int num
+    | Some x -> String.concat "" x
+;;
+
+let (--) i j =
+    let rec aux acc n =
+        if n < i then acc else aux (n::acc) (n-1)
+    in aux [] (j-1)
+;;
+
+let fizzbuzzer = List.map fizzbuzz
+;;
+
+print_string ((1 -- 64) |> fizzbuzzer |> String.concat "\n")
