364ff1be0f03606355c55c15fb1675899487e22f
mrillusi0n <csbnikhil@gmail.com>
2021-08-08 06:27:07 +0530

    initial approach (!works)

---
+++ challenges/coins.ml
@@ -0,0 +1,13 @@
+let rec solve required_denoms denoms change =
+    if change <= 0 then Some required_denoms
+    else match denoms with
+    | [] -> None
+    | denom::rest -> let rd,c = if denom < change then 
+        (denom::required_denoms,change-denom)
+        else (required_denoms,change)
+    in solve rd denoms c
+;;
+
+let dispense denoms =
+    solve [] (List.sort compare denoms |> List.rev)
+;;
