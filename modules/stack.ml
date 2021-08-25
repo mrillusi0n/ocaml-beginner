06b32443b58dce39b54eebd7571273babb3c0cdc
theteachr <sop.theteachr@gmail.com>
2021-08-25 06:39:34 +0530

    use `option`

--- modules/stack.ml
+++ modules/stack.ml
@@ -1,23 +1,28 @@
 module type Stack = sig
-    type 'a stack
+    type 'a t
 
-    val empty : 'a stack
+    val empty : 'a t
 
-    val is_empty    : 'a stack -> bool
-    val push        : 'a -> 'a stack -> 'a stack
-    val pop         : 'a stack -> 'a stack
-    val peek        : 'a stack -> 'a
-end
+    val is_empty    : 'a t -> bool
+    val push        : 'a -> 'a t -> 'a t
+    val pop         : 'a t -> 'a t option
+    val peek        : 'a t -> 'a option
+end;;
 
 module ListStack : Stack = struct
-    type 'a stack = 'a list
+    type 'a t = 'a list
 
     let empty = []
 
-    let is_empty s  = (s = [])
-    let push x s    = x::s
-    let pop         = function | [] -> failwith "Empty" | _::xs -> xs
-    let peek        = function | [] -> failwith "Empty" | x::_ -> x
-end
+    let is_empty s = (s = [])
 
+    let push x s = x::s
 
+    let pop = function
+        | [] -> None
+        | _::xs -> Some xs
+
+    let peek = function
+        | [] -> None
+        | x::_ -> Some x
+end;;
