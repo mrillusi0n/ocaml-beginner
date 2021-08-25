b44ceec825915a6ce2ca573bb94d31bc0a11530e
theteachr <sop.theteachr@gmail.com>
2021-08-25 06:39:57 +0530

    add ;;s

--- ads/bst.ml
+++ ads/bst.ml
@@ -3,6 +3,7 @@ type 'a tree = Leaf | Node of 'a * 'a tree * 'a tree
 let rec mem x = function
     | Leaf -> false
     | Node (v,l,r) -> x = v || (x < v && mem v l) || mem v r
+;;
 
 let rec insert x  = function
     | Leaf -> Node (x, Leaf, Leaf)
@@ -10,3 +11,4 @@ let rec insert x  = function
             if x = v then t
             else if x < v then Node (v, insert x l, r)
             else Node (v, l, insert x r)
+;;
--- basics.ml
+++ basics.ml
@@ -1,12 +1,26 @@
 (* simple math *)
 
 let double n = n * 2
+;;
+
 let square n = n * n
+;;
+
 let cube n = n ** 3.
+;;
+
 let sign n = if n = 0 then 0 else if n > 0 then 1 else -1
+;;
+
 let area_circle r = 3.14 *. r ** 2.
+;;
+
 let root_mean_square x y = ((x ** 2. +. y ** 2.) /. 2.) ** 0.5
+;;
+
 let divide (numerator:float) (denominator:float) = numerator /. denominator
+;;
+
 
 (* printing *)
 
@@ -15,7 +29,8 @@ let rec print_int_list = function
     | h::t -> print_int h; 
               print_newline ();
               print_int_list t
+;;
 
 let print_int_list' lst = 
     List.iter (fun x -> print_int x;print_newline ();) lst
-
+;;
