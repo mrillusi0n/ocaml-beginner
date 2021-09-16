57a14737fdd0782cc024c654651ed7d933194a16
Nikhil CSB <csbnikhil@gmail.com>
2021-09-17 05:15:28 +0530

    pack (failed)

---
+++ ninety-nine/pack.ml
@@ -0,0 +1,9 @@
+let pack list =
+  let rec aux curr acc = function
+    | [] -> acc
+    | [x] -> (x :: curr) :: acc
+    | x :: (y :: ys as t) ->
+        if x = y then aux (x :: curr) acc t
+        else aux [] ((x::curr)::acc) t
+  in List.rev (aux [] [] list)
+;;
