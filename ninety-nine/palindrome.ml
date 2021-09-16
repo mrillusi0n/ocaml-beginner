2ec439566a9a1563f8a8b5b05670c5d63b4a46a3
Nikhil CSB <csbnikhil@gmail.com>
2021-09-17 02:16:53 +0530

    use equality between lists

--- ninety-nine/palindrome.ml
+++ ninety-nine/palindrome.ml
@@ -1,6 +1,3 @@
-let is_palindrome list =
-    let rec aux = function
-        | x::xs, y::ys -> if x = y then aux (xs, ys) else false
-        | _ -> true
-    in (list, rev list) |> aux
+(* using the [rev] function defined in reverse.ml *)
+let is_palindrome list = list = rev list
 ;;
