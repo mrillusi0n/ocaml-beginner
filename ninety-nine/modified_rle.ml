083c2fecbbf2c7d14b80ca0bffc5fbc34f703beb
Nikhil CSB <csbnikhil@gmail.com>
2021-09-17 23:19:32 +0530

    implement decode

--- ninety-nine/modified_rle.ml
+++ ninety-nine/modified_rle.ml
@@ -1,14 +1,28 @@
 type 'a rle =
-  | One of 'a
-  | Many of int * 'a;;
+	| One of 'a
+	| Many of int * 'a;;
 
 let encode list =
-  let rec aux run acc = function
-    | [] -> acc
-    | [x] -> (if run = 0 then One x else Many (run+1, x)) :: acc
-    | a :: (b :: _ as t) ->
-        if a = b then aux (run+1) acc t
-        else let r = if run = 0 then One a else Many (run+1, a)
-        in aux 0 (r :: acc) t
-  in List.rev (aux 0 [] list)
+	let rec aux run acc = function
+		| [] -> acc
+		| [x] -> (if run = 0 then One x else Many (run+1, x)) :: acc
+		| a :: (b :: _ as t) ->
+			if a = b then aux (run+1) acc t
+			else let r = if run = 0 then One a else Many (run+1, a)
+			in aux 0 (r :: acc) t
+	in List.rev (aux 0 [] list)
+;;
+
+
+let decode list =
+  let rec cons times e acc =
+    if times = 0 then acc
+    else cons (times-1) e (e::acc)
+  in 
+	let rec aux acc = function
+		| [] -> acc
+		| x::xs -> (match x with
+		| One a -> aux (a::acc) xs
+		| Many (count, b) -> aux (cons count b acc) xs)
+	in List.rev (aux [] list)
 ;;
