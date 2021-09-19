78da299b6e71a72807b1392e389b709a001842af
Nikhil CSB <csbnikhil@gmail.com>
2021-09-20 00:04:42 +0530

    implement `%`

--- ninety-nine/rotate.ml
+++ ninety-nine/rotate.ml
@@ -1,4 +1,15 @@
+let sign = function
+  | p when p > 0 -> `Positive
+  | n when n < 0 -> `Negative
+  | _ -> `Zero 
+;;
 
+let (%) x y =
+  match sign x with
+  | `Positive -> x mod y
+  | `Negative -> y - (-x mod y)
+  | `Zero -> 0
+;;
 
 let rotate list n =
   let rec split acc list n = (* [n] < length of the list *)
@@ -6,6 +17,6 @@ let rotate list n =
     | x::xs, i when i > 0 -> split (x::acc) xs (n-1)
     | _ -> List.rev acc, list
   in
-  let (left, right) = split [] list n in
+  let (left, right) = split [] list (n % (List.length list)) in
   List.rev_append (List.rev right) left
 ;;
