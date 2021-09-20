eca8f4ec94192c5a247a0b2e335256782a6d79f5
Nikhil CSB <csbnikhil@gmail.com>
2021-09-21 02:53:14 +0530

    shorter default args

--- ninety-nine/range.ml
+++ ninety-nine/range.ml
@@ -1,8 +1,8 @@
-let xrange ?step:(s=1) ?left:(l=0) right =
-  let rec aux acc l =
-    if l > right then acc
-    else aux (l::acc) (l+s)
-  in List.rev (aux [] l)
+let xrange ?(step=1) ?(left=0) right =
+  let rec aux acc left =
+    if left > right then acc
+    else aux (left::acc) (left+step)
+  in List.rev (aux [] left)
 
 let range init term =
   if init < term then xrange ~left:init term
