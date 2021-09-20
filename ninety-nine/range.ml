26f7351822274bf282a2113f845414ad6dfd28ad
Nikhil CSB <csbnikhil@gmail.com>
2021-09-20 21:52:25 +0530

    define xrange

--- ninety-nine/range.ml
+++ ninety-nine/range.ml
@@ -1,6 +1,10 @@
+let xrange ?step:(s=1) ?left:(l=0) right =
+  let rec aux acc l =
+    if l > right then acc
+    else aux (l::acc) (l+s)
+  in List.rev (aux [] l)
+
 let range init term =
-  let rec aux acc curr =
-    if curr < init then acc
-    else aux (curr::acc) (curr-1)
-  in aux [] term
+  if init < term then xrange ~left:init term
+  else List.rev (xrange ~left:term init)
 ;;
