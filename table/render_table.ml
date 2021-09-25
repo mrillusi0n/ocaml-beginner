c27c90425dde1c80c7f36922a08f07772c3a63e2
Nikhil CSB <csbnikhil@gmail.com>
2021-09-25 10:08:03 +0530

    extract fun into a named function

--- table/render_table.ml
+++ table/render_table.ml
@@ -22,9 +22,8 @@ let make_row widths row =
 
 let max_widths header rows =
   let lengths = List.map ~f:String.length in
-  List.fold_left
-    rows ~init:(lengths header)
-    ~f:(fun res row -> List.map2_exn res (lengths row) Int.max)
+  let max_lengths res row = List.map2_exn res (lengths row) Int.max in
+  List.fold_left rows ~init:(lengths header) ~f:max_lengths
 
 let render_table header rows =
   let widths = max_widths header rows in
