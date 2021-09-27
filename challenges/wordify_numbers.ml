93fe78bc252864ecf912d3c20b22ef36da61c8ad
Nikhil CSB <csbnikhil@gmail.com>
2021-09-28 03:32:27 +0530

    major fix

--- challenges/wordify_numbers.ml
+++ challenges/wordify_numbers.ml
@@ -1,3 +1,6 @@
+type grouping = Indian | International
+;;
+
 let digit_to_word = function
   | 1 -> "one"
   | 2 -> "two"
@@ -57,13 +60,17 @@ let num_to_digit_words n =
   |> String.concat ~sep:" "
 ;;
 
-let group_three list =
+let group system list =
+  let divisor = match system with
+  | International -> 3
+  | Indian -> 2
+  in
   let rec aux curr acc n = function
     | [] ->
         if List.is_empty curr then acc else curr::acc
     | x::xs ->
       let (curr', acc') =
-        if n % 3 = 0 then ([], (x::curr)::acc) else (x::curr, acc)
+        if n % divisor = 0 then ([], (x::curr)::acc) else (x::curr, acc)
       in
       aux curr' acc' (n+1) xs
   in
@@ -78,15 +85,19 @@ let face_place_pair_to_word = function
 
 let rec face_place_pairs_to_words acc = function
   | [] -> acc
-  | [(digit, _)] | [(0, _); (digit, _)] -> digit_to_word digit :: acc
+  | (0, _) :: rest -> face_place_pairs_to_words acc rest
+  | [(digit, _)] -> digit_to_word digit :: acc
   | [(tens, _); (0, _)] -> tenth_to_word tens :: acc
   | [(1, _); (ones, _)] -> eleven_to_twenty_to_word (10 + ones) :: acc
   | [(tens, _); (ones, _)] ->
-      (tenth_to_word tens ^ " " ^ digit_to_word ones) :: acc
+      (digit_to_word ones) :: (tenth_to_word tens) :: acc
   | pair :: rest ->
       face_place_pairs_to_words (face_place_pair_to_word pair :: acc) rest
 ;;
 
+let append_place_word place l =
+  if place = 0 then l
+  else (place_to_word (10 ** (3 * place))) :: l
 
 let wordify_number = function
   | 0 -> "zero"
@@ -94,17 +105,20 @@ let wordify_number = function
     |> extract_digits
     |> List.rev
     |> List.rev_mapi ~f:(fun i digit -> (digit, (10 ** (i % 3))))
-    |> group_three
-    |> List.mapi ~f:(fun i group -> face_place_pairs_to_words [] group)
+    |> group International
+    |> List.rev_mapi ~f:(fun i group -> face_place_pairs_to_words [] group)
+    |> List.filter_mapi ~f:(fun i words ->
+        if List.is_empty words then None
+        else Some (append_place_word i words))
     |> List.rev_map ~f:List.rev
     |> List.map ~f:(String.concat ~sep:" ")
-    |> List.mapi ~f:(fun i words -> words ^ " " ^ (place_to_word (10 ** (3 * i))))
-    |> List.rev_map ~f:String.strip
     |> String.concat ~sep:" "
 ;;
 
-let number = 123456
+let tests = [123456; 1000; 1042; 65534; 300000; 5000000; 1000000000]
 ;;
 
-let test = wordify_number number
+let res = tests
+    |> List.map ~f:wordify_number
+    |> List.zip_exn tests
 ;;
