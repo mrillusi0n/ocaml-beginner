7b6589d0c4db78e8b498bff6b39dcef3fe6d71de
Nikhil CSB <csbnikhil@gmail.com>
2021-11-06 01:18:19 +0530

    implement trivial solution

--- challenges/dec_to_hex.ml
+++ challenges/dec_to_hex.ml
@@ -17,28 +17,14 @@ let hex_table = [|
 ; "F"
 |] ;;
 
-let dec_to_bin n =
+let dec_to_hex n =
   let rec aux acc x =
     if x = 0 then acc
-    else aux (x % 2 :: acc) (x / 2)
+    else aux (x % 16 :: acc) (x / 16)
   in
-  if n = 0 then [0] else aux [] n
-;;
-
-let bin_list_to_dec =
-  Fn.compose snd (List.fold_left ~f:(fun (i, acc) bit -> (i+1, acc + (2 ** i) * bit)) ~init:(0, 0))
-;;
-
-let dec_to_hex n =
-  let open List in
-  n
-  |> dec_to_bin
-  |> rev
-  |> groupi ~break:(fun i _ _ -> i % 4 = 0)
-  |> map ~f:(Fn.compose (fun i -> hex_table.(i)) bin_list_to_dec)
-  |> rev
+  (if n = 0 then [0] else aux [] n)
+  |> List.map ~f:(fun x -> hex_table.(x))
   |> String.concat
 ;;
 
 let hex_twenty = dec_to_hex 20 ;;
-let bin_twenty = dec_to_bin 20 ;;
