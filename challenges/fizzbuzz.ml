9119c461328b4573b547dd5290fa0300dfd6bf13
Nikhil CSB <csbnikhil@gmail.com>
2021-11-28 14:10:12 +0530

    make vocable pairs a constant

--- challenges/fizzbuzz.ml
+++ challenges/fizzbuzz.ml
@@ -1,22 +1,25 @@
-let fizzbuzz vocable_pairs n =
+let vocable_pairs = [
+	(7,"Bazz");
+	(5,"Buzz");
+	(3,"Fizz");
+]
+
+
+let fizzbuzz n =
 	let rec aux acc = function
 		| [] -> acc
 		| (d, v) :: rest -> aux (if n % d = 0 then v :: acc else acc) rest 
 	in
 	match aux [] vocable_pairs with
 	| [] -> Int.to_string n
-	| l -> String.concat l
+	| ls -> String.concat ls
 
 
 let (--) init term =
 	List.init (term - init) (( + ) init)
 
 
-let fizzbuzzer = List.map ~f:(fizzbuzz [
-	(7,"Bazz");
-	(5,"Buzz");
-	(3,"Fizz");
-])
+let fizzbuzzer = List.map ~f:fizzbuzz
 
 
 let test = fizzbuzzer (1 -- 64)
