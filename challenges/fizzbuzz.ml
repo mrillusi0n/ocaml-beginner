447f4d7aa02675376ac29aa19c9d819ed6af780b
Nikhil CSB <csbnikhil@gmail.com>
2021-11-28 14:01:08 +0530

    remove unnecessary type

--- challenges/fizzbuzz.ml
+++ challenges/fizzbuzz.ml
@@ -1,17 +1,11 @@
-type word = Number of int | Vocable of string 
-
-let string_of_word = function
-	| Number n -> Int.to_string n
-	| Vocable v -> v
-
 let fizzbuzz vocable_pairs n =
 	let rec aux acc = function
 		| [] -> acc
 		| (d, v) :: rest -> aux (if n % d = 0 then v :: acc else acc) rest 
 	in
-	let res = aux [] vocable_pairs in
-	(if List.is_empty res then Number n else Vocable (String.concat res))
-	|> string_of_word
+	match aux [] vocable_pairs with
+	| [] -> Int.to_string n
+	| l -> String.concat l
 
 let (--) init term =
 	List.init (term - init) (( + ) init)
