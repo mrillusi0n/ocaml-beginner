4d728b53d50b2839e3fadaa3de02e4bbe932430f
Nikhil CSB <csbnikhil@gmail.com>
2021-12-07 22:22:33 +0530

    improved range

--- ninety-nine/range.ml
+++ ninety-nine/range.ml
@@ -1,10 +1,3 @@
-let xrange ?(step=1) ?(left=0) right =
-  let rec aux acc left =
-    if left > right then acc
-    else aux (left::acc) (left+step)
-  in List.rev (aux [] left)
-
-let range init term =
-  if init < term then xrange ~left:init term
-  else List.rev (xrange ~left:term init)
-;;
+let range a b =
+  let op = if a <= b then ( + ) else ( - ) in
+  List.init ((abs (a - b)) + 1) (op a)
